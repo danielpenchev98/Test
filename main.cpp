7633342587b135650843dca5f09f1db7155d25ba
Daniel Penchev <42358431+danielpenchev98@users.noreply.github.com>
2018-11-16 22:20:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -108,6 +108,92 @@ int main()
     q5.AddOption("Tablicata na stranicite e dopulnena s bit za prisustvie, pokazvasht dali suotvetnata stranica se namira v svobodnata pamet", 0);
     q5.AddOption("Tablicata na stranicite se pazi nqkude v operativnata pamet", 0);
     exam.push_back(q5);
+    //------------------
+      Question d0("Koi ot trite metoda na razpolagane na blokove ot operativnata pamet se implementira nai-lesno(izberete edno)?");
+    d0.AddOption("Direktno supostavqne", 0);
+    d0.AddOption("Pulna asociativnost", 0);
+    d0.AddOption("Mnojestvena asociativnost", 0);
+    exam.push_back(d0);
+    
+    Question d1("Otbelejete nevqrnoto tvurdenie(izberete edno).");
+    d1.AddOption("Dlavnata (DRAM) pamet e po-bavna ot SRAM pametta", 0);
+    d1.AddOption("DRAM konsumira po-malko energiq ot SRAM", 0);//tuka rusalchoto go smeni,moje i da e greshno
+    d1.AddOption("Cache pametta e malka,burza i SRAM bazirana", 0);
+    d1.AddOption("SRAM e po-evtina ot DRAM", 0);
+    exam.push_back(d1);
+    
+    Question d2("Kakva e korelaciqta kapacitet i burzodeistvie pri kompiyturnite pameti(izberete edno)?");
+    d2.AddOption("Proporcionalna", 0);
+    d2.AddOption("Obratnoproporcionalna", 0);//verniqt
+    d2.AddOption("Nqma korelaciq", 0);
+    exam.push_back(d2);
+   
+    Question d3("Kakuv e formata na liniqta pri Cache pametta s mnojestvena acociativnost(izberete edno)?");
+    d3.AddOption("tag,duma", 0);
+    d3.AddOption("tag,liniq,duma", 0);
+    d3.AddOption("tag,mnojestwo,duma", 0);//verniqt
+    exam.push_back(d3);
+    
+    Question d4("Koi vid pameti sa po-burzi ot Cache pametite(izberete edno)?");
+    d4.AddOption("Registrite", 0);//tova e verniqt otgovor
+    d4.AddOption("Flash pametite", 0);
+    d4.AddOption("Glavnata pamet", 0);
+    exam.push_back(d4);
+    
+    Question d5("Metodut pri koito obnovqvane na glavnata pamet nastupva asinhronno sled premahvane na duma ot Cache se naricha(izberete edno)?");
+    d5.AddOption("Zashtiten zapis", 0);
+    d5.AddOption("Write-back", 0);//verniqt
+    d5.AddOption("Cache-write", 0);
+      d5.AddOption("Write-trough", 0);
+    exam.push_back(d5);
+    
+    Question d6("Kakva e formata na liniqta pri Cache pamet s pulna asociativnost (izberete edno)?");
+    d6.AddOption("tag,liniq,duma", 0);
+    d6.AddOption("tag,duma", 0);
+    d6.AddOption("tag,mojestvo,duma", 0);
+    exam.push_back(d6);
+    
+    Question d7("Dadena e mashina s pobaitovo adresiruema osnovna pamet s razemr 2^16 baita,i razmer na bloka 8 baita.Cache pametta e s \ndirektna organizaciai se sustoi ot 32 linii.Kakuv e maksimalniqt broi baitove, koito mogat da budat razpolojeni w Cache pametta?(izberete edno)?");
+    d7.AddOption("256B", 0);//verniqt
+    d7.AddOption("1024B", 0);
+    d7.AddOption("512B", 0);
+     d7.AddOption("128B", 0);
+     d7.AddOption("64B", 0);
+    exam.push_back(d7);
+    
+    Question d8("LFU e algoritumut za(izberete edno)?");
+    d8.AddOption("zamestvane na nai-chesto izpolzvanata stranica ot Cache pametta ", 0);
+    d8.AddOption("zamestvane na nai-neizpolzwanata stranica v Cache pametta", 0);//veren
+    d8.AddOption("zamestvane na nqkoi ot izpolzvanite stranici v Cache pametta", 0);
+     d8.AddOption("periodichno izchistvane priznacite za obrushtenie kum nai-rqdko izpolzvanite stranici v Cache pametta.", 0);
+    exam.push_back(d8);
+    
+     Question d9("Dadena e mashina s pobaitowo adresiruema osnovna pamet i 2-kraten mnojestveno asociativen Cache. Cache-logikata interpretira adresa ot pametta\nkakto sledva :14 bita za tag, 8 nita za mnojestvo,2 bita za adresirane na duma\n.Kolko e maksimalniq broi na blokovete v glavnata pamet?(izberete edno)?");
+    d9.AddOption("Ne moje da bude opredelen", 0);
+    d9.AddOption("2^2", 0);
+    d9.AddOption("2^8", 0);
+     d9.AddOption("2^22", 0);//veren
+     d9.AddOption("2^14", 0);
+       d9.AddOption("2^24", 0);
+    exam.push_back(d9);
+    
+     Question d10("Cache pamet s asociativna organizaciq e s kapacitet 64 linii,pazdeleni na mnojestva s po 16 linii vsqko.Osnovnata pamet sudurja 4K bloka s po128 dumi vseka.\nPosochete adresniqt format ,koito i suotvetstva:(Formata e : tag,set,word)(izberete edno)?");
+    d10.AddOption("8 2 7", 0);//towa bi trqbwalo da e vqrno , but who knows, definitely not Cvetanov
+    d10.AddOption("14 8 4", 0);
+    d10.AddOption("14 8 7", 0);
+     d10.AddOption("4 8 7", 0);
+    exam.push_back(d10);
 
+      Question d11("Metodut,pri koito zapis na dannite se izvurshva samo v Cache pametta,a sustoqnieto na Cache-bloka se otbelqzva \nkato obiknoveno v ypravlqvashtoto pole s priznak,narechen \"update\" ili \"dirty\" se naricha :(izberete edno)?");
+    d11.AddOption("Obraten zapis(Write back)", 0);
+    d11.AddOption("Ednovremenen zapis(Write trough)", 0);
+    exam.push_back(d11);
+    
+    Question d10("Cache pamet s asociativna organizaciq e s kapacitet 64 linii,pazdeleni na mnojestva s po 16 linii vsqko.Osnovnata pamet sudurja 4K bloka s po128 dumi vseka.\nPosochete adresniqt format ,koito i suotvetstva:(Formata e : tag,set,word)(izberete edno)?");
+    d10.AddOption("8 2 7", 0);//towa bi trqbwalo da e vqrno , but who knows, definitely not Cvetanov
+    d10.AddOption("14 8 4", 0);
+    d10.AddOption("14 8 7", 0);
+     d10.AddOption("4 8 7", 0);
+    exam.push_back(d10);
     return 0;
 }
